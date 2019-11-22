09eda092a885bf261429e790bad5518f80e82708
rachfuerst <rach.fuerst@gmail.com>
2019-11-22 13:02:10 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: :index
+  # Call the Rails #resources method on the :students resource
+  # Only call the :show action on this resource
+  # We will then configure the #show method in the StudentsController 
+  # to give the show.html.erb file access to the application instance variables.
+  resources :students, only: :show
 end
